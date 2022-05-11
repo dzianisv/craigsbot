ed768fc2f561d97d9596876022ab65f66cf4fee4
Dennis Vashchuk <dennis.vashchuk@gmail.com>
2022-05-11 00:38:25 -0700

    Add /list and /help commands

--- src/bot.py
+++ src/bot.py
@@ -14,17 +14,27 @@ def start(u, ctx):
     })
     u.message.reply_text('Subscribed')
 
+def list(u, ctx):
+    urls = [ doc['url'] for doc in db.subscribers.find({'telegram_chat_id': u.message.chat_id }) ]
+    u.message.reply_text(f"Subscribed to {urls}")
+
 def stop(u, ctx):
     logging.info("unsubscribing %d", u.message.chat_id)
     db.subscribers.delete_many({
         'telegram_chat_id': u.message.chat_id
     })
-    u.message.reply_text('Unsubscribed')
+    u.message.reply_text('Unsubscribed from all')
+
+def help(u, ctx):
+    u.message.reply_text('/start <url>\tget updates on a new ad at <url>\n/stop\tunsubscribe from the all updated\n/list\tlist all the subscribtions\n')
+
 
 def telegram_events_loop():
     updater = telegram.ext.Updater(token=config.telegram_token, use_context=True)
     updater.dispatcher.add_handler(telegram.ext.CommandHandler('start', start))
     updater.dispatcher.add_handler(telegram.ext.CommandHandler('stop', start))
+    updater.dispatcher.add_handler(telegram.ext.CommandHandler('list', list))
+    updater.dispatcher.add_handler(telegram.ext.CommandHandler('help', help))
     updater.start_polling()
     updater.idle()
     
